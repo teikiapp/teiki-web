0fbcd768d4e3c0b8a30663a93f47b63133eb4db6
Peter Adison <cpeteradison@gmail.com>
2026-06-30 02:46:58 +0800

    feat: full pages redesign to dark themed

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './footer.html',
 })
 export class Footer {}
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,8 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { Seo } from '../../services/seo';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
   templateUrl: './home.html',
 })
 export class Home {
