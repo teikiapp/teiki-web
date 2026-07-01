b1d1da5491a63e9201a31c53ac9ebeeed2ca5fc3
Peter Adison <cpeteradison@gmail.com>
2026-07-01 14:34:40 +0800

    refactor: remove footer links

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink, RouterLinkActive],
   templateUrl: './footer.html',
 })
 export class Footer {}
